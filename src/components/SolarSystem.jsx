45c3c6349788fbefd6643f9251ef604e4784244e
Raypcte <rayanevalak@gmail.com>
2022-07-24 21:06:10 -0300

    questao 1

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Title from './Title';
+
+class App extends React.Componet {
+  render() {
+    <div>
+      <header><h1>Solar Sistem</h1></header>
+
+      <title headline={"headlineText"} />
+    </div>;
+  }
+}
+export default App;
---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import Title from './Title';
+
+class Title extends React.Componet {
+  render() {
+    <div>
+    <title headline={'headlineText'}>
+      <h2>headline</h2>
+    </title>;
+    </div>
+  }
+}
+export default Title;
